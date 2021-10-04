ec79dfc4d83de14af7d50e63fe7362d318654ba1
Tamara <tamaragmartin@gmail.com>
2021-10-04 19:42:18 +0200

    app and main config

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
+import {Route, Switch} from 'react-router-dom';
+
+import Home from "../Home";
+import Error from "../Error/Error";
+
+
 class Main extends Component {
   render() {
-    return <div>Main</div>;
+    return ( 
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route component={Error} />
+      </Switch>
+    </main>
+    );
   }
 }
 
