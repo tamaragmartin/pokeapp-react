a0f815c4742737918aae287aef511ebf41b74205
Tamara <tamaragmartin@gmail.com>
2021-10-06 11:48:00 +0200

    sass config

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="header">
       {/* <img src={logo} alt="newspaper" className="header--logo" /> */}
-      <h1>TechCrunch's News Feed</h1>
+      <h1>PokeAPP</h1>
       <Nav />
     </header>
   )
