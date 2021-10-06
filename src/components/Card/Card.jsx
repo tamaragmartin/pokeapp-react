4796df0a8838d5c0ec0561e5f56bd76594c0fdee
Tamara <tamaragmartin@gmail.com>
2021-10-06 10:04:27 +0200

    second version finalized

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export const Card = () => {
+
+const Card = (props) => {
+  const pokemon = props.data;
   return (
-    <div>
-      Card
-    </div>
-  )
+    
+    <>
+    <section>
+      <article>
+        <p>{pokemon ? pokemon.name : ""}</p>
+        <p>#{pokemon ? pokemon.id : ""}</p>
+      </article>
+    </section>
+    <section>
+        <article>
+          <p> {pokemon.stats ? pokemon.stats[0].base_stat : ""}</p>
+          <p> {pokemon.types ? pokemon.types[0].type.name : ""}</p>
+          <p> {pokemon ? pokemon.weight : ""}</p>
+          <p> {pokemon ? pokemon.height : ""}</p>
+        </article>
+        <article>
+          {pokemon.sprites ? (
+            <img id="image"
+              src={pokemon.sprites.other["official-artwork"]["front_default"]}
+              alt={pokemon.name} />
+          ) : (
+            ""
+          )}
+        </article>
+
+      </section>
+      </>
+  );
+
 }
 
-export default Card;
+export default Card
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import ListaPokemon from "../ListaPokemon";
 
-import Card from '../Card'
+import { userContext } from "../../context/userContext";
+// import useFetch from "../../hooks/fetch-hook";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const { pokemons, setPokemon } = useContext(userContext);
+  const inputSearch = useRef(null);
+
+   const fetchData = () => {
+     (async () => {
+       const resp = await axios.get(
+         "https://pokeapi.co/api/v2/pokemon/" + input
+       );
+       setPokemon([...pokemons, resp.data]);
+     })();
+   };
+
+  // const {loading,result} = useFetch("https://pokeapi.co/api/v2/pokemon"+input)
+  //   useEffect(async ()=>{
+  //     setPokemon([...pokemons, result.results])
+  //   }, [result])
+
+   const handleChange = (e) => {
+     setInput(e.target.value);
+   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  //borrar texto del input
+   useEffect(() => {
+     inputSearch.current.value = "";
+     setInput("");
+   }, [pokemons]);
+  //  const inputs = document.querySelectorAll("input");
+  //  inputs.forEach((input) => (input.value = ""));
+
   return (
     <>
       <section>
-        <form action="">
-          <input type="text" name="search" />
-          <button>Buscar</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Find a Pokemon"
+            type="text"
+            name="search"
+            ref={inputSearch}
+            onChange={handleChange}
+          />
+          <button onClick={fetchData}>Search</button>
         </form>
       </section>
       <section>
-        <Card />
+        <ListaPokemon />
       </section>
     </>
-  )
-}
+  );
+};
 
 export default Home;
