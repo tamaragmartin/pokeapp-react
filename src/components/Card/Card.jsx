1f9110ae5c120cbf4fc36e257d9e5e316bdc9547
Tamara <tamaragmartin@gmail.com>
2021-10-06 10:28:01 +0200

    second version updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,29 +8,24 @@ const Card = (props) => {
     <>
     <section>
       <article>
-        <p>{pokemon ? pokemon.name : ""}</p>
-        <p>#{pokemon ? pokemon.id : ""}</p>
+        <p>{pokemon.name.toUpperCase()} </p>
+        <p>Pokédex ID: #{pokemon.id}</p>
+        <p>Weight: {pokemon.weight}</p>
+        <p>Height: {pokemon.height}</p>
+        <p>Type: {pokemon.types[0].type.name}</p>
+        <p>Popular move: {pokemon.moves[2].move.name}</p>
+      </article>
+      <article>
+        {pokemon.sprites ? (
+          <img id="image"
+            src={pokemon.sprites.other["official-artwork"]["front_default"]} //front default muy pequeño
+            alt="" />
+        ) : (
+          ""
+        )}
       </article>
     </section>
-    <section>
-        <article>
-          <p> {pokemon.stats ? pokemon.stats[0].base_stat : ""}</p>
-          <p> {pokemon.types ? pokemon.types[0].type.name : ""}</p>
-          <p> {pokemon ? pokemon.weight : ""}</p>
-          <p> {pokemon ? pokemon.height : ""}</p>
-        </article>
-        <article>
-          {pokemon.sprites ? (
-            <img id="image"
-              src={pokemon.sprites.other["official-artwork"]["front_default"]}
-              alt={pokemon.name} />
-          ) : (
-            ""
-          )}
-        </article>
-
-      </section>
-      </>
+    </>
   );
 
 }
